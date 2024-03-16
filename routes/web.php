a7b527084e5022ffe0cdb3232fad1d407c8f760c
Thanh Diep <dangvanthanhdiep2711@gmail.com>
2024-03-16 20:43:06 +0700

    v2

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use App\Http\Controllers\Admin\ChinaController;
 use App\Http\Controllers\Admin\CountryController;
+use App\Http\Controllers\Admin\IndiaController;
+use App\Http\Controllers\Admin\JapanController;
+use App\Http\Controllers\Admin\JoinController;
+use App\Http\Controllers\Admin\MemberjoinController;
 use App\Http\Controllers\Admin\MountainController;
+use App\Http\Controllers\Admin\NepanController;
+use App\Http\Controllers\Admin\PakistanController;
 use App\Http\Controllers\Admin\VietnamController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
@@ -51,6 +57,42 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('destroy/{id}', 'destroy')->name('destroy');
     });
 
+    Route::prefix('user')->name('user.')->controller(UserController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+
+        Route::get('create', 'create')->name('create');
+        Route::post('store', 'store')->name('store');
+
+        Route::get('edit/{id}', 'edit')->name('edit');
+        Route::post('update/{id}', 'update')->name('update');
+
+        Route::get('destroy/{id}', 'destroy')->name('destroy');
+    });
+
+    Route::prefix('join')->name('join.')->controller(JoinController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+
+        Route::get('create', 'create')->name('create');
+        Route::post('store', 'store')->name('store');
+
+        Route::get('edit/{id}', 'edit')->name('edit');
+        Route::post('update/{id}', 'update')->name('update');
+
+        Route::get('destroy/{id}', 'destroy')->name('destroy');
+    });
+
+    Route::prefix('memberjoin')->name('memberjoin.')->controller(MemberjoinController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+
+        Route::get('create', 'create')->name('create');
+        Route::post('store', 'store')->name('store');
+
+        Route::get('edit/{id}', 'edit')->name('edit');
+        Route::post('update/{id}', 'update')->name('update');
+
+        Route::get('destroy/{id}', 'destroy')->name('destroy');
+    });
+
     Route::prefix('vietnam')->name('vietnam.')->controller(VietnamController::class)->group(function () {
         Route::get('index', 'index')->name('index');
     });
@@ -58,4 +100,21 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::prefix('china')->name('china.')->controller(ChinaController::class)->group(function () {
         Route::get('index', 'index')->name('index');
     });
+
+    Route::prefix('japan')->name('japan.')->controller(JapanController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+    });
+
+    Route::prefix('nepan')->name('nepan.')->controller(NepanController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+    });
+
+    Route::prefix('pakistan')->name('pakistan.')->controller(PakistanController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+    });
+
+    Route::prefix('india')->name('india.')->controller(IndiaController::class)->group(function () {
+        Route::get('index', 'index')->name('index');
+    });
+
 });
