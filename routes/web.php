b4f8060aad9ba2711b9a80143361df7ba6a7b777
Thanh Diep <dangvanthanhdiep2711@gmail.com>
2024-03-16 21:07:17 +0700

    v3

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\MemberjoinController;
 use App\Http\Controllers\Admin\MountainController;
 use App\Http\Controllers\Admin\NepanController;
 use App\Http\Controllers\Admin\PakistanController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\VietnamController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
@@ -28,6 +29,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('admin/blog',[BlogController::class ,'blog'])->name('admin.blog');
+
+Route::get('admin/vietnam/index',[VietnamController::class ,'index'])->name('admin.vietnam.index');
+
+Route::get('admin/china/index',[ChinaController::class ,'index'])->name('admin.china.index');
+
+Route::get('admin/janan/index',[JapanController::class ,'index'])->name('admin.janan.index');
+
+Route::get('admin/nepan/index',[NepanController::class ,'index'])->name('admin.nepan.index');
+
+Route::get('admin/pakistan/index',[PakistanController::class ,'index'])->name('admin.pakistan.index');
+
+Route::get('admin/india/index',[IndiaController::class ,'index'])->name('admin.india.index');
 
 
 
@@ -93,28 +106,5 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('destroy/{id}', 'destroy')->name('destroy');
     });
 
-    Route::prefix('vietnam')->name('vietnam.')->controller(VietnamController::class)->group(function () {
-        Route::get('index', 'index')->name('index');
-    });
-
-    Route::prefix('china')->name('china.')->controller(ChinaController::class)->group(function () {
-        Route::get('index', 'index')->name('index');
-    });
-
-    Route::prefix('japan')->name('japan.')->controller(JapanController::class)->group(function () {
-        Route::get('index', 'index')->name('index');
-    });
-
-    Route::prefix('nepan')->name('nepan.')->controller(NepanController::class)->group(function () {
-        Route::get('index', 'index')->name('index');
-    });
-
-    Route::prefix('pakistan')->name('pakistan.')->controller(PakistanController::class)->group(function () {
-        Route::get('index', 'index')->name('index');
-    });
-
-    Route::prefix('india')->name('india.')->controller(IndiaController::class)->group(function () {
-        Route::get('index', 'index')->name('index');
-    });
 
 });
